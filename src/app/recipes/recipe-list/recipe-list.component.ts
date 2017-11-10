db6730567cbfbc68bc66ffaeb929650b4ec8b81d
mserino <margherita.serino@accenture.com>
2017-11-10 18:12:44 +0100

    Recipe functionality

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output
+} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,17 +13,28 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   // The only thing that will be stored in  recipes is an array of Recipe
   recipes: Recipe[] = [
     new Recipe(
       'Sushi',
       'Delicious rolls made of rice and fish',
-      'https://halfoff.adspayusa.com/wp-content/uploads/2017/04/sushi_and_sashimi_for_two.0.jpg')
+      'https://halfoff.adspayusa.com/wp-content/uploads/2017/04/sushi_and_sashimi_for_two.0.jpg'),
+
+    new Recipe(
+      'Uramaki',
+      'Inside-out rolls made of salmon and avocado',
+      'https://monstersushi.es/71-preview_default/sake-avocado.jpg'
+    )
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
