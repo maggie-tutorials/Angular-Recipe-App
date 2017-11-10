3e841f1af93e404b7413b40596af7137db05e0f0
mserino <margherita.serino@accenture.com>
2017-11-10 18:28:25 +0100

    Adding an ingredient from shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 
@@ -18,4 +23,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
